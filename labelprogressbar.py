78a1071b6bb57c30bec6d7c2882b444010c9b3cc
Bert McDowell <bertmcdowell@gmail.com>
2020-03-04 14:16:05 +0000

    Update labelprogressbar.py
    Move over to use a thread for the update and added support to auto set the label with the percentage of progress.

--- labelprogressbar.py
+++ labelprogressbar.py
@@ -2,7 +2,9 @@
 
 # Copyright (c) Bert McDowell 2020
 # For license see LICENSE
-from threading import Timer
+from threading import Thread
+from threading import Condition
+from threading import Lock
 
 from tkinter import ttk
 from tkinter import *
@@ -10,9 +12,6 @@ from tkinter import *
 __version__ = "0.1"
 __author__  = "Bert McDowell <bertmcdowell@gmail.com>"
 __all__     = ["LabelProgressbar"]
-
-HORIZONTAL = 'horizontal'
-VERTICAL = 'vertical'
 
 class LabelProgressbar(ttk.Frame):
     __inititialized = False
@@ -30,7 +29,10 @@ class LabelProgressbar(ttk.Frame):
 
         self._is_running = False
         self._timer = None
-        self._interval = .02
+        self._condition = Condition()
+        self._lock = Lock()
+        self._intervalDefault = 50 / 1000
+        self._interval = self._intervalDefault
 
         self._frame = ttk.Frame(master=self, relief=RIDGE)
         self._frame.pack(fill=BOTH, expand=True, pady=2, padx=2)
@@ -42,52 +44,107 @@ class LabelProgressbar(ttk.Frame):
         self._bar = self._canvas.create_rectangle(0, 0, 100, 40, fill = "lightgray")
         self._label = self._canvas.create_text(4, 4, anchor="nw", angle=0)
 
+        self._value = kwargs.pop('value', 100)
+        self._maximum = kwargs.pop('maximum', 100)
+        self._step = kwargs.pop('step', 1.0)
+        self._text = kwargs.pop('text', '%')
+
         if kwargs.pop('orient', HORIZONTAL) == HORIZONTAL:
             self._orient = HORIZONTAL
         else:
             self._orient = VERTICAL
 
-        self._progress = 0
-        self._length = kwargs.pop('length', 100)
-        self._step = kwargs.pop('step', 1)
+        self._updateText()
 
     def _getProgress(self):
-        return self._progress / self._length
+        return self._value / self._maximum
 
-    def _update(self):
+    def _updateBar(self):
         progress = self._getProgress()
         if self._orient == HORIZONTAL:
             self._canvas.coords(self._bar, 2, 2, (self._canvas.winfo_width() - 3) * progress, self._canvas.winfo_height() - 3)
-            self._canvas.coords(self._label, 4, (self._canvas.winfo_height() - 3) * 0.5)
         else: 
             self._canvas.coords(self._bar, 2, 2, self._canvas.winfo_width() - 3, (self._canvas.winfo_height() - 3) * progress)
 
+    def _updateLabel(self):
+        if self._orient == HORIZONTAL:
+            self._canvas.coords(self._label, (self._canvas.winfo_height() - 3) * 0.5, (self._canvas.winfo_height() - 3) * 0.5)
+            self._canvas.itemconfig(self._label, angle=0)
+        else: 
+            self._canvas.coords(self._label, (self._canvas.winfo_height() - 3) * 0.5, (self._canvas.winfo_height() - 3) * 0.5)
+            self._canvas.itemconfig(self._label, angle=90)
+
+    def _updateText(self):
+        if self._text == '%':
+            self._canvas.itemconfig(self._label, text='{:03d}%'.format(int(self._getProgress() * 100)))
+            self._updateLabel()
+
     def _configure(self, event):
-        self._update()
+        self._updateBar()
+        self._updateLabel()
 
     def _run(self):
-        if self.is_running:
-            self._progress = (self._progress + self._step) % self._length
-            self._update()
+        self._condition.acquire()
+        while self._is_running:
+            self.step()
+            self._condition.wait(self._interval)
+        self._condition.release()
 
-            self._timer = Timer(self._interval, self._run)
-            self._timer.start()
-        else:
-            self._timer = None
+    def start(self, interval=None):
+        """Begin autoincrement mode: schedules a recurring timer event that calls 
+           Progressbar.step() every interval milliseconds. 
+           If omitted, interval defaults to 50 milliseconds."""
+        if interval is not None and isinstance(interval, (int, float, complex)):
+            self._interval = interval / 1000
 
-    def start(self):
         if not self._is_running:
-            self._timer = Timer(self._interval, self._run)
+            self._is_running = True
+            self._timer = Thread(target=self._run)
             self._timer.start()
-            self.is_running = True
+            self.event_generate("<<ProgressbarStart>>", data={"widget" : self})
+
+    def step(self, amount=None):
+        """Increments the progress bar’s value by amount.
+            amount defaults to 1.0 if omitted."""
+        self._lock.acquire()
+        if amount is not None and isinstance(amount, (int, float, complex)):
+            self._value += amount
+        else:
+            self._value += self._step
+        if self._value > self._maximum:
+            self._value = 0.0
+        elif self._value < 0.0:
+            self._value = self._maximum
+        self._updateBar()
+        self._updateText()
+        self._lock.release()
+        self.event_generate("<<ProgressbarStep>>", data={"widget" : self})
 
     def stop(self):
-        self._is_running = False
+        """Stop autoincrement mode: cancels any recurring timer event initiated 
+           by Progressbar.start() for this progress bar."""
+        if self._timer and self._timer.is_alive():
+            self._is_running = False
+            try:
+                self._condition.notify()
+            except RuntimeError as exception:
+                print(exception)
+            self._timer.join(self._interval)
+        self._interval = self._intervalDefault
+        self._timer = None
 
     def set(self,text):
-        self._canvas.itemconfig(self._label, text=text)
-        self._update()
+        self._lock.acquire()
+        self._text=text
+        if self._text == '%':
+            self._updateText()
+        else:
+            self._canvas.itemconfig(self._label, text=text)
+            self._updateLabel()
+        self._lock.release()
 
+    def get(self):
+        return self._canvas.itemconfig(self._label, option="text")
 
 # Test:
 def _test():
@@ -95,10 +152,16 @@ def _test():
     root=Tk()
     root.title("Example")
     progressbar=LabelProgressbar(root)
-    progressbar.set("Text")
     progressbar.pack(fill=BOTH,expand=True, padx=2, pady=2)
 
-    progressbar.start()
+    startbtn = Button(text="Start", width=30, command= lambda progressbar=progressbar: progressbar.start())
+    startbtn.pack(side=RIGHT, padx=2)
+
+    stepbtn = Button(text="Step", width=30, command= lambda progressbar=progressbar: progressbar.step())
+    stepbtn.pack(side=RIGHT, padx=2)
+
+    stopbtn = Button(text="Stop", width=30, command= lambda progressbar=progressbar: progressbar.stop())
+    stopbtn.pack(side=RIGHT, padx=2)
 
     root.geometry('1080x1080')
     root.mainloop()
